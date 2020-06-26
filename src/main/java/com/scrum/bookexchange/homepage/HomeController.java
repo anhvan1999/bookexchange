24cf85592cede1253316443cbac8eba58a9ff674
Van Duc Vo <vanducvo.dev@gmail.com>
2020-06-26 22:42:34 +0700

    Add common fragment include common_head is library and common_header is nvabar

--- src/main/java/com/scrum/bookexchange/homepage/HomeController.java
+++ src/main/java/com/scrum/bookexchange/homepage/HomeController.java
@@ -1,15 +1,19 @@
 package com.scrum.bookexchange.homepage;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("")
 public class HomeController {
     
     @GetMapping("")
-    public String getHome() {
+    public String getHome(Principal principal, Model model) {
+        model.addAttribute("username", principal != null ? principal.getName() : null);
         return "home";
     }
 
--- src/main/java/com/scrum/bookexchange/security/config/SecurityConfig.java
+++ src/main/java/com/scrum/bookexchange/security/config/SecurityConfig.java
@@ -32,7 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers(
+                        "/",
+                        "/js/**",
+                        "/css/**"
+                )
+                .permitAll()
                 .anyRequest().authenticated()
             .and()
                 .formLogin()
