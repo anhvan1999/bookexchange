d9ea0a3340de0a973aaba405fa8491751f0a0fde
Van Duc Vo <vanducvo.dev@gmail.com>
2020-07-01 20:15:12 +0700

    GUI and render upload page

---
+++ src/main/java/com/scrum/bookexchange/uploadbook/UploadBookController.java
@@ -0,0 +1,16 @@
+package com.scrum.bookexchange.uploadbook;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class UploadBookController {
+    @GetMapping("/upload")
+    public String getUpload(Principal principal, Model model){
+        model.addAttribute("username", principal != null ? principal.getName() : null);
+        return "upload";
+    }
+}
