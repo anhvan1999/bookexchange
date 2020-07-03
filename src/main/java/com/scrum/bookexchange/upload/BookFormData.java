a3ad8556a3e4132ba64ab97b76fcea6a44edbd65
lltphs <40392252+lltphs@users.noreply.github.com>
2020-07-03 11:31:29 +0700

    blah

--- src/main/java/com/scrum/bookexchange/upload/BookFormData.java
+++ src/main/java/com/scrum/bookexchange/upload/BookFormData.java
@@ -1,7 +1,13 @@
 package com.scrum.bookexchange.upload;
 import lombok.Data;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 @Data
 public class BookFormData {
-    private String name;
+    private String title;
+    private List<MultipartFile> images;
+    private List<String> options;
 }
--- src/main/java/com/scrum/bookexchange/upload/UploadController.java
+++ src/main/java/com/scrum/bookexchange/upload/UploadController.java
@@ -3,14 +3,20 @@ package com.scrum.bookexchange.upload;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.scrum.bookexchange.security.entity.Book;
-import com.scrum.bookexchange.security.repos.BookRepos;
+import com.scrum.bookexchange.book.entity.Book;
+import com.scrum.bookexchange.book.entity.BookOption;
+import com.scrum.bookexchange.book.repos.BookRepos;
+import com.scrum.bookexchange.book.repos.ImageRepos;
+import com.scrum.bookexchange.book.repos.OptionRepos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/upload")
@@ -29,37 +35,39 @@ public class UploadController {
                     Pattern.CANON_EQ |
                     Pattern.CASE_INSENSITIVE |
                     Pattern.UNICODE_CASE);
-
-    @GetMapping(
-        path = "/register",
-        consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}
-    )
+    
     public boolean validationName(String name){
         Matcher namePatternMatcher = namePattern.matcher(name);
         return namePatternMatcher.find();
     }
-    public String uploadBook(BookFormData form){
+    @PostMapping(
+        path = "/upload",
+        consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}
+    )
+    public String uploadBook(Principal principal, BookFormData form){
         try{
-            if(!this.validationName(form.getName())){
+            if(!this.validationName(form.getTitle())){
                 return "redirect:/upload?invalid=name";
             }
-
-            Book book = Book.builder()
-                    .name(form.getName())
-                    .build();
-            bookRepos.save(book);
-            for (int i=0;i<form.getOption().length;i++){
-                Option option = Option.builder()
-                                .wayToExchange(form.getOption()[i])
-                                .build();
-                optionRepos.save(option);
-            }
-            for (int i=0;i<form.getImage().length;i++){
-                Image image = Image.builder()
-                                .image(form.getImage()[i])
-                                .build();
-                imageRepos.save(image);
-            }
+            
+            // Book book = Book.builder()
+            //         .title(form.getTitle())
+            //         .owner(principal.getName())
+            //         .build();
+            // bookRepos.save(book);
+            // for (int i=0;i<form.getOptions().size();i++)
+            //     BookOption option = BookOption.builder()
+            //                     .book(form.getTitle())
+            //                     .optionString(form)
+            //                     .build();
+            //     optionRepos.save(option);
+            // }
+            // for (int i=0;i<form.getImage().length;i++){
+            //     Image image = Image.builder()
+            //                     .image(form.getImage()[i])
+            //                     .build();
+            //     imageRepos.save(image);
+            // }
             return "redirect:/upload?result=success";
         } catch (Exception e) {
             return "redirect:/upload?error=true";
