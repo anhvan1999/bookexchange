3b1e539a4e2a5a9531748d9c45b2b2e5f0ebb3d7
Van Duc Vo <vanducvo.dev@gmail.com>
2020-07-04 12:36:26 +0700

    add link to homepage

--- src/main/java/com/scrum/bookexchange/security/config/SecurityConfig.java
+++ src/main/java/com/scrum/bookexchange/security/config/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/img/**",
                         "/css/**",
                         "/register",
-                        "/login"
+                        "/login",
+                        "/image/**"
                 )
                 .permitAll()
                 .anyRequest().authenticated()
