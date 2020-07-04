6704ccaa0a3b68a56df6c54ef60c39f9b563c330
Van Duc Vo <vanducvo.dev@gmail.com>
2020-07-04 13:23:24 +0700

    ok

--- src/main/java/com/scrum/bookexchange/book/controller/BookDetailController.java
+++ src/main/java/com/scrum/bookexchange/book/controller/BookDetailController.java
@@ -1,5 +1,11 @@
 package com.scrum.bookexchange.book.controller;
 
+import com.scrum.bookexchange.book.entity.Book;
+import com.scrum.bookexchange.book.entity.BookOption;
+import com.scrum.bookexchange.book.repos.BookRepos;
+import com.scrum.bookexchange.book.repos.OptionRepos;
+import com.scrum.bookexchange.book.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,17 +13,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class BookDetailController {
+    @Autowired
+    public BookService service;
+
+    @Autowired
+    private BookRepos bookRepos;
+
+    @Autowired
+    private OptionRepos optionRepos;
 
     @GetMapping(
             value = "/book/{id}"
     )
     public String getDetailBook(@PathVariable("id") String id, Principal principal, Model model){
-        model.addAttribute("username", principal != null ? principal.getName() : null);
         Long lid = Long.parseLong(id);
-        System.out.println(id);
+        List<Book> bookOfUser = service.getUserBook();
+        Book currentBook = bookRepos.findById(lid).orElseThrow();
+        List<BookOption> bookOption = optionRepos.findByBook(currentBook);
+
+        model.addAttribute("username", principal != null ? principal.getName() : null);
+        model.addAttribute("bookOfUser", bookOfUser);
+        model.addAttribute("current", currentBook);
+        model.addAttribute("bookOption", bookOption);
+
         return "book";
     }
 }
