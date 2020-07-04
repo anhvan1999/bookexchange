4155fd80a9b46c9e189ba5ce333be7482da2d701
Anh Van <danganhvan1999@gmail.com>
2020-07-04 12:46:06 +0700

    Update

--- src/main/java/com/scrum/bookexchange/book/repos/BookRepos.java
+++ src/main/java/com/scrum/bookexchange/book/repos/BookRepos.java
@@ -1,10 +1,14 @@
 package com.scrum.bookexchange.book.repos;
 
+import java.util.List;
+
 import com.scrum.bookexchange.book.entity.Book;
+import com.scrum.bookexchange.security.entity.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepos extends JpaRepository<Book, Long> {
-    
+    List<Book> findByOwner(User user);
 }
---
+++ src/main/java/com/scrum/bookexchange/book/service/BookService.java
@@ -0,0 +1,31 @@
+package com.scrum.bookexchange.book.service;
+
+import java.util.List;
+
+import com.scrum.bookexchange.book.entity.Book;
+import com.scrum.bookexchange.book.repos.BookRepos;
+import com.scrum.bookexchange.security.entity.User;
+import com.scrum.bookexchange.security.repos.UserRepos;
+import com.scrum.bookexchange.security.service.BookExchangeUserDetails;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class BookService {
+
+    private final UserRepos repos;
+
+    private final BookRepos bookRepos;
+
+    public List<Book> getUserBook() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = repos.findByEmail(username).orElseThrow();
+        List<Book> listBook = bookRepos.findByOwner(user);
+        return listBook;
+    }
+
+}
